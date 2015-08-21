0c140d5a20cdd490e524eee875ced540a5e64378
Tessa <Tessachi33@yahoo.com>
2015-08-21 10:22:04 -0700

    Added new word route and word from vtl, passes

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -16,12 +16,12 @@ public class App {
      return new ModelAndView(model, layout);
    }, new VelocityTemplateEngine());
 
- //   get("/addressbooks/new", (request, response) -> {
- //     HashMap<String, Object> model = new HashMap<String, Object>();
- //     model.put("template", "templates/addressbook-form.vtl");
- //     return new ModelAndView(model, layout);
- //   }, new VelocityTemplateEngine());
- //
+   get("/words/new", (request, response) -> {
+     HashMap<String, Object> model = new HashMap<String, Object>();
+     model.put("template", "templates/word-form.vtl");
+     return new ModelAndView(model, layout);
+   }, new VelocityTemplateEngine());
+
  //   post("/addressbooks", (request, response) -> {
  //     HashMap<String, Object> model = new HashMap<String, Object>();
  //     String userName = request.queryParams("userName");
